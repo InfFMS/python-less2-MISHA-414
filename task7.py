36ed67c87848b68624facadafcb8f25d3579a55e
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2024-09-30 14:15:59 +0700

    Задача 7

--- task7.py
+++ task7.py
@@ -1,8 +1,15 @@
 n=int(input())
-simple=[i for i in range(2, n+1)]
-for i in range(n-1):
+simple=[i for i in range(2, n)]
+for i in range(n//2):
     a=simple[i]
-    count=0
-    for k in range(i):
-        if a%k!=0:
-            simple
+    if a!=0:
+        for i_nul in range(i+a, n//2, a):
+            simple[i_nul]=0
+answer=[]
+for num in range((n-2)//2):
+    if simple[num]!=0:
+        answer.append(simple[num])
+answer.append(n)
+# answer=answer.append(n)
+print(answer)
+print(simple)
